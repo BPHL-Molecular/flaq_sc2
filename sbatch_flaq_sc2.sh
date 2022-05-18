30715542fd042473581e82c4460374f3ba260197
SESchmedes <sarahschmedes@gmail.com>
2022-05-18 15:12:32 -0400

    upload beds reference and edit sbatch

--- sbatch_flaq_sc2.sh
+++ sbatch_flaq_sc2.sh
@@ -16,4 +16,4 @@ module load singularity
 
 primer_version="4.1"
 
-python flaq_sc2.py fastqs/ --primer_bed /blue/bphl-florida/share/references/sars-cov-2/primers/ARTIC-V${primer_version}.bed --lib_frag frag --threads $SLURM_CPUS_ON_NODE --ref_fasta /blue/bphl-florida/share/references/sars-cov-2/reference/nCoV-2019.reference.fasta --ref_gff /blue/bphl-florida/share/references/sars-cov-2/reference/GCF_009858895.2_ASM985889v3_genomic.gff --sotc S:L452R,S:E484K  --pango_path /apps/staphb-toolkit/containers/pangolin_4.0.5-pdata-1.3.sif --pangolin v4.0.5 --pangolin_data v1.3 
+python flaq_sc2.py fastqs/ --primer_bed primers/ARTIC-V${primer_version}.bed --lib_frag frag --threads $SLURM_CPUS_ON_NODE --ref_fasta reference/nCoV-2019.reference.fasta --ref_gff reference/GCF_009858895.2_ASM985889v3_genomic.gff --sotc S:L452R,S:E484K  --pango_path /apps/staphb-toolkit/containers/pangolin_4.0.5-pdata-1.3.sif --pangolin v4.0.5 --pangolin_data v1.3 
